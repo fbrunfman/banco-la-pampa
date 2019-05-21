fc3a14b01f85a3acedce3d5f69f38b8f1d4dad66
Sebastian Saenz <sebastian.saenz@vusecurity.com>
2019-05-20 21:58:16 -0300

    Agrego método para cambiar contraseña

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,26 +9,6 @@ class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        // $http = new \GuzzleHttp\Client;
-        // try {
-        //     $response = $http->post('http://localhost:8000/oauth/token', [
-        //         'form_params' => [
-        //             'grant_type' => 'password',
-        //             'client_id' => '2',
-        //             'client_secret' => 'lGQXRysJhZAZ16nMp7p1waab88sl25fcygNeua9M',
-        //             'username' => $request->username,
-        //             'password' => $request->password
-        //         ]
-        //     ]);
-        //     return $response->getBody();
-        // } catch (\GuzzleHttp\Exception\BadResponseException $e) {
-        //     if ($e->getCode() === 400) {
-        //         return response()->json('Invalid Request. Please enter a username or a password.', $e->getCode());
-        //     } else if ($e->getCode() === 401) {
-        //         return response()->json('Your credentials are incorrect. Please try again', $e->getCode());
-        //     }
-        //     return response()->json('Something went wrong on the server.', $e->getCode());
-        // }
 
         $username = $request->username;
         $password = $request->password;
@@ -45,10 +25,6 @@ class AuthController extends Controller
             'post'
         );
         $response = Route::dispatch($tokenRequest);
-
-        // if($response->getStatusCode() == 200){
-        //     $this->storeAccessToken($response->getContent());
-        // }
 
         return $response;
     }
